62dd4a59fb5a74a758cd13d80c9fe2749945407b
Desislava Topuzakova <34713586+DesislavaTopuzakova@users.noreply.github.com>
2024-09-28 10:09:16 +0300

    For Loop

---
+++ 07-For-Loop/Demos/Demo_Strings_Chars/Program.cs
@@ -0,0 +1,15 @@
+﻿string name = "Desislava";
+
+//всеки един символ от клавиатурата се намира ASCII Table
+//всеки един символ си има ascii код (int)
+
+char symbol = 'D';
+Console.WriteLine((int)symbol);
+
+int number = 65;
+Console.WriteLine((char) number);
+
+
+char letter = (char) 97;
+int price = 'r';
+
---
+++ 07-For-Loop/Solutions/LatinLetters_07/Program.cs
@@ -0,0 +1,16 @@
+﻿//входни данни
+char startSymbol = char.Parse(Console.ReadLine()); //символ, който е начало на диапазона
+char endSymbol = char.Parse(Console.ReadLine()); //символ, който е край на диапазона
+
+//всеки символ от startSymbol до endSymbol -> отпечатвам го и слагам интервал след него
+//повтаряне на действие определен брой пъти -> for цикъл
+
+//1. начало -> startSymbol
+//2. край -> endSymbol
+//3. повтаряме -> отпечатваме символа и след това интервал
+//4. промяна -> +1
+
+for (char symbol = startSymbol; symbol <= endSymbol; symbol++)
+{
+    Console.Write(symbol + " ");
+}
